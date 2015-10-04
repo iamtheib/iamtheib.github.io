d5732d3500fae220501b3628c8b212888a921e4f
Ibrahim Sikiru <ib@iamtheib.me>
2015-10-04 01:32:32 -0400

    updated navbar interaction

--- _assets/javascripts/script.js
+++ _assets/javascripts/script.js
@@ -1,46 +1,67 @@
 $(document).ready(function() {
 
-    $(".navbar-collapse").height($("#main").height());
+    function closeSidebarMenu() {
+        $.sidr('close', 'nav-menu');
+    }
+
+    function hideSearchField() {
+        $('.search-container').hide();
+        $('.search-btn').show();
+        $('#search-results').hide();
+        $('#search-field').unbind('keyup');
+    }
+
+    function showSearchField() {
+        $('.search-btn').hide();
+        $('.search-container').show();
+        $('#search-field').val('').focus();
+        $('#search-field').bind('keyup', function(event) {
+            // key code 27 is for ESC
+            if (event.keyCode === 27) {
+                hideSearchField();
+            }
+        });
+    }
 
     $('.burger-menu').sidr({
    	    name: 'nav-menu',
-  		side: 'right',
-  		speed: 400,
-  		forceclick: true,
-  		onOpen: function() {
+    		side: 'right',
+    		speed: 400,
+    		forceclick: true,
+    		onOpen: function() {
   				  $('.burger-menu').hide();
   				  $('.search-icon').hide();
-  				},
-  		onClose: function() {
+				},
+    		onClose: function() {
   				  $('.burger-menu').show();
-  				  if (!$('.search-field').is(':visible')) {
-  				  	$('.search-icon').show();
+            // only show the search icon if the search field is hidden
+  				  if (!$('.search-container').is(':visible')) {
+    				  	$('.search-icon').show();
   				  }
-  				}
-	});
+				}
+  	});
 
-	$('.close-menu').click(function(e) {
-		e.preventDefault();
-		$.sidr('close', 'nav-menu');
-	});
-
-	$('.search-btn').click(function(e) {
-		$('.search-btn').hide();
-		$('.search-field').show()
-		$('#search-query').val('').focus();
-	});
-
-	$('.close-search').click(function(e) {
-		$('.search-field').hide();
-		$('.search-btn').show();
-		$('#search-results').hide();
-	});
-
-	$('#search-query').lunrSearch({
-      indexUrl: '/assets/index.json',
-      results : '#search-results',
-      entries : '.entries',
-      template: '#search-results-template'
+    $('#search-field').lunrSearch({
+        indexUrl: '/assets/index.json',
+        results : '#search-results',
+        entries : '.entries',
+        template: '#search-results-template'
     });
+
+    // Sidebar should have the height of the main page content
+    $(".navbar-collapse").height($("#main").height());
+
+  	$('.close-menu').click(function(e) {
+    		closeSidebarMenu();
+  	});
+
+  	$('.search-btn').click(function(e) {
+        closeSidebarMenu();
+        showSearchField();
+  	});
+
+  	$('.close-search').click(function(e) {
+    		hideSearchField();
+  	});
 
 });
