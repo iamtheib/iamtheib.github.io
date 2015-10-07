08b65db1b950c8ee5fb65c288d73cd03f0b1e983
Ibrahim Sikiru <ib@iamtheib.me>
2015-10-06 20:18:39 -0400

    removed project post content and search

--- _assets/javascripts/script.js
+++ _assets/javascripts/script.js
@@ -4,25 +4,6 @@ $(document).ready(function() {
         $.sidr('close', 'nav-menu');
     }
 
-    function hideSearchField() {
-        $('.search-container').hide();
-        $('.search-btn').show();
-        $('#search-results').hide();
-        $('#search-field').unbind('keyup');
-    }
-
-    function showSearchField() {
-        $('.search-btn').hide();
-        $('.search-container').show();
-        $('#search-field').val('').focus();
-        $('#search-field').bind('keyup', function(event) {
-            // key code 27 is for ESC
-            if (event.keyCode === 27) {
-                hideSearchField();
-            }
-        });
-    }
-
     $('.burger-menu').sidr({
    	    name: 'nav-menu',
     		side: 'right',
@@ -30,38 +11,17 @@ $(document).ready(function() {
     		forceclick: true,
     		onOpen: function() {
   				  $('.burger-menu').hide();
-  				  $('.search-icon').hide();
 				},
     		onClose: function() {
   				  $('.burger-menu').show();
-            // only show the search icon if the search field is hidden
-  				  if (!$('.search-container').is(':visible')) {
-    				  	$('.search-icon').show();
-  				  }
 				}
   	});
-
-    $('#search-field').lunrSearch({
-        indexUrl: '/assets/index.json',
-        results : '#search-results',
-        entries : '.entries',
-        template: '#search-results-template'
-    });
 
     // Sidebar should have the height of the main page content
     $(".navbar-collapse").height($("#main").height());
 
   	$('.close-menu').click(function(e) {
     		closeSidebarMenu();
-  	});
-
-  	$('.search-btn').click(function(e) {
-        closeSidebarMenu();
-        showSearchField();
-  	});
-
-  	$('.close-search').click(function(e) {
-    		hideSearchField();
   	});
 
 });
--- _posts/projects/2015-07-11-thats-the-word.md
+++ _posts/projects/2015-07-11-thats-the-word.md
@@ -1,42 +1,8 @@
 ---
+permalink: /
 title: that's the word
-project-id: thats-the-word
-layout: project
 img: thats-the-word-img.png
-start-date: May 2014
 src: https://github.com/iamtheib/thats-the-word
-language: Java
 dist: thats-the-word.zip
 category: projects
 ---
-
-This was my first 'non-single-source-file' project I worked on,
-that wasn't a school project.
-
-It was during my second year in university and I was taking this
-introductory course to data structures.
-
-<!--more-->
-
-There is this phone app, called 4 Pics 1 Word, that I would use to pass
-time. If you don't know what that is, it's a game where you're
-shown 4 pictures, a bunch of letters (I forgot how many), a set amount
-of spaces (where you will put your answer), and your task is to fill the
-spaces with letters from the ones you're given, to form a word that
-describes all four pictures.... fun stuff
-
-I got to some pictures that I had no idea what they could possibly have
-in common, or my guesses were just wrong. I ended up having to spend some
-coins (virtual ofcourse), to unlock the answers, and after seeing them,
-I thought, 'Obviously'..... It was easy to recognize the answer once you
-see it.
-
-To cut the story short, I ended up making something to help me beat
-this game by showing me all the possible answers, and by using my
-awesome recognition skill, I could pick out the right one. Errmm...
-it's not cheating if you still have to recognise the answer..... I think..
-
-Ofcourse, my first implementation was very basic, with just ArrayLists
-storing the possible words and searching through that. However, after
-learning a bit more about data structures, I switched to using a Trie
-instead.
